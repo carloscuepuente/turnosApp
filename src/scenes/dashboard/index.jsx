4ee2af695e68b9140bd885051a17a829d5be68a0
Carlos Cue <cacp892@gmail.com>
2023-03-17 13:44:06 +0100

    comented some lines to pass netlify build validation

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,7 @@
 
-import React, { useState } from 'react';
-import { Button, Modal, TextField, Grid, useTheme, Box, Container } from '@mui/material';
-import { tokens } from "../../theme";
+import React from 'react';
+// import { Button, Modal, TextField, Grid, useTheme, Box, Container } from '@mui/material';
+// import { tokens } from "../../theme";
 
 
 export default function Dashboard() {
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -9,17 +9,17 @@ import { tokens } from "../../theme";
 
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 // componente que sirve para pasarle a las cosas que necesita react-pro-sidebar
 const Item = ({ title, to, icon, selected, setSelected }) => {
